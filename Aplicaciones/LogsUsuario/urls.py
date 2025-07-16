aa37ffe01892fff5fbbde96aa6a33e05bddbf516
Alex Iza <axel.iza2003@gmail.com>
2025-07-16 18:03:30 -0500

    funcionamiento logs compelto

--- Aplicaciones/LogsUsuario/urls.py
+++ Aplicaciones/LogsUsuario/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import eliminar_log_usuario
+from . import views
+
 
 urlpatterns = [
+    path('editar_log_usuario/<int:id>/', views.editar_log_usuario, name='editar_log_usuario'),
     path('eliminar_log_usuario/<int:id>/', eliminar_log_usuario, name='eliminar_log_usuario'),
 ]
